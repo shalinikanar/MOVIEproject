5493cab5182c67d01e26070f151b061332cd866d
shalini kanar <shalinikanar@hotmail.com>
2018-10-09 18:55:46 -0400

    I will be working on Find the movie by actor and also quit program.

---
+++ MOVIEproject/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOVIEproject
+{
+   public class Movie
+    {
+        private string Name;
+        private string mainActor;
+        private string Genre;
+        private string Director;
+
+        public Movie(string Name, string mainActor, string Genre, string Director)
+
+        {
+            this.Name = Name;
+            this.mainActor = mainActor;
+            this.Genre = Genre;
+            this.Director = Director;
+        }
+    }
+}
